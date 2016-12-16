7bd9a06fb6cf1840b54a1bb099b3e631769ec4fd
Nathan Chancellor <natechancellor@gmail.com>
2016-12-15 20:10:10 -0700

    Revert "video: mdss: LiveDisplay driver"
    This doesn't play well with KCAL so let's add flar2's implementation.
    
    This reverts commit 3eeef10ac8c0f9ab1132832ea6317149861453d7.

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -23,7 +23,6 @@
 #include <linux/err.h>
 #include <linux/display_state.h>
 #include "mdss_dsi.h"
-#include "mdss_livedisplay.h"
 
 #ifdef CONFIG_WAKE_GESTURES
 #include <linux/wake_gestures.h>
@@ -169,7 +168,7 @@ u32 mdss_dsi_panel_cmd_read(struct mdss_dsi_ctrl_pdata *ctrl, char cmd0,
 	return 0;
 }
 
-void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+static void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
 			struct dsi_panel_cmds *pcmds)
 {
 	struct dcs_cmd_req cmdreq;
@@ -717,8 +716,6 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	if (mdss_bl_ctrl_by_panel())
 		mdss_dsi_panel_bl_ctrl(pdata, bl_default_lvl);
 
-	mdss_livedisplay_update(ctrl, MODE_UPDATE_ALL);
-
 end:
 	pinfo->blank_state = MDSS_PANEL_BLANK_UNBLANK;
 	pr_debug("%s:-\n", __func__);
@@ -842,7 +839,7 @@ static void mdss_dsi_parse_trigger(struct device_node *np, char *trigger,
 }
 
 
-int mdss_dsi_parse_dcs_cmds(struct device_node *np,
+static int mdss_dsi_parse_dcs_cmds(struct device_node *np,
 		struct dsi_panel_cmds *pcmds, char *cmd_key, char *link_key)
 {
 	const char *data;
@@ -1930,8 +1927,6 @@ static int mdss_panel_parse_dt(struct device_node *np,
 	if (mdss_bl_ctrl_by_panel())
 		of_property_read_u32(np, "qcom,bl-default-lvl",
 						&bl_default_lvl);
-
-	mdss_livedisplay_parse_dt(np, pinfo);
 
 	return 0;
 
